f30a192f5fd08eb4228e9198832fac0812a79f95
easydong02 <easydong02@gmail.com>
2025-01-20 22:58:54 +0900

    [2025-01-20] go - web scrapper2

--- webscrapper/main.go
+++ webscrapper/main.go
@@ -1,19 +1,80 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	"log"
 	"net/http"
+	"os"
 )
 
-var baseURL string = "https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query=go+docs"
+var baseURL string = "https://search.naver.com/search.naver?nso=&page=2&query=go+docs&sm=tab_pge&"
+
+type extractedResult struct {
+	url string
+}
 
 func main() {
-	getPages()
+	var allUrls []extractedResult
+	pages := getPages()
+	fmt.Println(pages)
+
+	for i := 0; i < pages/2; i++ {
+		urls := getPage(i)
+		allUrls = append(allUrls, urls...)
+	}
+
+	writeUrls(allUrls)
+}
+
+func writeUrls(urls []extractedResult) {
+	file, err := os.Create("urls.csv")
+	checkErr(err)
+
+	w := csv.NewWriter(file)
+	defer w.Flush()
+
+	headers := []string{"URL"}
+
+	wErr := w.Write(headers)
+	checkErr(wErr)
+
+	for _, url := range urls {
+		urlSlice := []string{url.url}
+		uwErr := w.Write(urlSlice)
+		checkErr(uwErr)
+	}
+}
+
+func getPage(pageNumber int) []extractedResult {
+	var results []extractedResult
+	pageURL := baseURL + fmt.Sprintf("start=%d&where=web", 1+(15*pageNumber))
+	fmt.Println("Requesting", pageURL)
+	res, err := http.Get(pageURL)
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	finds := doc.Find("a.link_tit")
+	finds.Each(func(i int, selection *goquery.Selection) {
+		val, exists := selection.Attr("href")
+		if exists {
+			result := extractedResult{url: val}
+			results = append(results, result)
+		}
+	})
+
+	fmt.Println("------------------------------------------")
+	return results
 }
 
 func getPages() int {
+	pages := 0
 	res, err := http.Get(baseURL)
 	checkErr(err)
 	checkCode(res)
@@ -32,11 +93,11 @@ func getPages() int {
 	}
 
 	fmt.Println("네이버에서 go docs 검색 결과")
-	doc.Find("div.sc_page div.sc_page_inner a").Each(func(i int, s *goquery.Selection) {
-		fmt.Println(s.Text())
+	doc.Find("div.sc_page div.sc_page_inner").Each(func(i int, s *goquery.Selection) {
+		pages = s.Find("a").Length()
 	})
 
-	return 0
+	return pages
 }
 
 func checkErr(err error) {
